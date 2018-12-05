d985a4dee110f5fad1477677d4c5f3c1925586d0
tejasvibelsare <tejasvibelsare@gmail.com>
2018-12-04 21:10:04 -0800

    Initial Commit

---
+++ CSC690iOS/CSC690iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC690iOS
+//
+//  Created by Tejasvi Belsare on 12/4/18.
+//  Copyright © 2018 Tejasvi Belsare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
